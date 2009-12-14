cb2a16e81dc96d80857e6de9f8e9d69d7bca8124
Antoine R. Dumont <eniotna.t@gmail.com>
2009-12-14 17:34:33 +0000

    CLEANING: Suppression de la synchronisation des fichiers de paramétrages. Seuls les fichiers de constantes exemples sont à synchroniser.

--- config/database.conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-//==============================================================================
-// Les informations pour la connexion à la base de données.
-//==============================================================================
-define('DATABASE_SERVER', "localhost");
-define('DATABASE_NAME', "baseeval");
-define('DATABASE_LOGIN', "baseeval");
-define('DATABASE_PASSWORD', "baseeval");
