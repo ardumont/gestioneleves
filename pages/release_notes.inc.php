7440ba9db7ca70e72b5f7399711ba0c6bbd168f9
Antoine R. Dumont <eniotna.t@gmail.com>
2010-06-19 08:47:00 +0000

    EVO: - Optimisation de code.

---
+++ pages/release_notes.inc.php
@@ -0,0 +1,299 @@
+<h2>Release notes</h2>
+<dl>
+	<dt>
+		<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+		Version v1
+	</dt>
+	<dd>
+		<a href="javascript:" onclick="showOrHideAllSubVersion(this);">Tout cacher/montrer</a>
+		<dl>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v2.0.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Gestion de la saisie de commentaires sur les périodes des élèves.</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.9.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Gestion des utilisateurs désactivés dans les différentes ihms.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Mise à jour des ihms de gestion des professeurs pour y ajouter la gestion du profil.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Mise en place des ihms d'édition, d'ajout, suppression des profils.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Création des profils 'Professeur' et 'Directeur/Directrice'.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Modification du message de fin d'installation pour préciser le login/mdp par défaut.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Mise à jour librairie javascript.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Factorisation de code pour amélioration de maintenance future.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Amélioration de la charte graphique</li>
+					<li class="bug_mineur" title="Bug mineur">Correction du mauvais domaine pour la matière "art".</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.8.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Amélioration de l'ergonomie générale de l'ihm.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Renommage des objets formulaires dans les différentes pages de l'application.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Reformatage du menu du module d'administration.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Reformatage du menu du module principal de l'application.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Mise en place des profils.</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.7.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout dans l'édition multiple de la possibilité de modifier l'évaluation collective des évaluations individuelles sélectionnées.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Simplification de la vue d'ajout des évaluations individuelles.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Amélioration de l'affichage au retour de la soumission d'un nouvel ajout.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'édition multiple à l'aide des checkbox de suppression multiple.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Gestion lors d'actions multiples de retours sur la page apres actions multiples avec les mêmes sélection qu'en partant.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm synthèse périodique d'une compétence pour une classe.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm d'impression synthèse périodique d'une compétence pour une classe.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm synthèse annuel d'une compétence pour une classe.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm d'impression synthèse annuel d'une compétence pour une classe.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Mise à jour de l'installation qui contient désormais tous les cycles/domaines/matieres/competences.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de liens permettant de (dé)sélectionner toutes les checkbox de suppression multiple dans la vue des évaluations individuelles.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout du critère de recherche sur une compétence dans la liste d'affichage des évaluations individuelles.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Critère de recherche sur une compétence dans la liste d'affichage des évaluations individuelles est filtré selon les cycles des classes du professeur.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Vue des évaluations individuelles : Critère de recherche sur la compétence est filtré sur les compétences déjà évaluées.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Vue de consultation périodique des compétences évaluées : Critère de recherche sur la compétence est filtré sur les compétences déjà évaluées.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Ajout de la saisie de commentaires rapides dans l'ajout d'évaluations individuelles.</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.6.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Amélioration de l'ergonomie des ihms.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Retrait de l'édition des valeurs des notes (pour figer le calcul des moyennes).</li>
+					<li class="evolution_majeure" title="Evolution majeure">Augmentation de la taille des fichiers d'imports xml.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm de visualisation des livrets périodiques des élèves d'une classe.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm d'impression des livrets périodiques des élèves d'une classe.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm de visualisation des livrets annuels des élèves d'une classe.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm d'impression des livrets annuels des élèves d'une classe.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm de visualisation des livrets cycliques des élèves d'une classe.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm d'impression des livrets cycliques des élèves d'une classe.</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.5.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Les livrets font désormais les moyennes.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Retrait de la suppression et de l'ajout des notes (pour le calcul des moyennes).</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm d'impression d'un livret annuel pour un élève.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm d'impression d'un livret périodique pour un élève.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm de visualisation de progression annuel ou cyclique d'un élève.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de l'ihm de visualisation de progression périodique pour un élève.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout du manuel du développeur (installation de plateforme, MPD, explication des sources, etc...)</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.4.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Amélioration de l'ergonomie des pages.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout de manuels sur certaines pages pour expliquer comment fonctionne la page.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout d'un menu d'administration d'import xml pour faciliter l'intégration des compétences (cycle, domaine, matières, compétences).</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout d'un menu d'administration d'import xml pour faciliter l'intégration des classes (classe, cycle, niveau, école, élèves).</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout d'un menu d'administration de listing des professeurs présents en bdd.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ajout d'un menu d'administration de listing des élèves par classe présents en bdd.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Mise en place du module de profil d'un professeur.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Mise en place d'un installeur graphique.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Refonte de la charte graphique.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Remise en place du projet sous subversion pour faciliter les développements.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Mise en place du module de déconnexion d'un professeur.</li>
+					<li class="evolution_majeure" title="Evolution mineure">Mise en place d'une page des contributeurs.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Mise en place d'un bloc de référencement des outils utilisés.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Refonte de la page d'accueil.</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.3.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Migration vers utf-8 (sources + bdd).</li>
+					<li class="evolution_mineure" title="Evolution mineure">Module de connexion du professeur.</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.2.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_mineure" title="Evolution mineure">Ihm de listing des p&eacute;riodes.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Ihm de listing des domaines.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Ihm de listing des mati&eacute;res.</li>
+					<li class="evolution_mineure" title="Evolution mineure">Ihm de listing des comp&eacute;tences.</li>
+					<li class="evolution_mineure" title="Evolution mineure">D&eacute;placement des modules d'administration dans le r&eacute;pertoire /admin.</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.1.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Ihm de listing et ajout d'&eacute;l&egrave;ves.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ihm de listing et ajout d'&eacute;coles.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ihm de listing et ajout de classes.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Ihm de listing des cycles et niveaux.</li>
+				</ul>
+			</dd>
+			<dt>
+				<img src="<?php echo(URL_ICONS_8X8); ?>/fold_off.gif" alt="[-]" title="Cacher" onclick="showOrHideVersion(this)" />&nbsp;
+				v1.0.0
+			</dt>
+			<dd>
+				<ul>
+					<li class="evolution_majeure" title="Evolution majeure">Mise en place des plateformes (apache, php, mysql, phpmyadmin, subversion, trac, testlink).</li>
+					<li class="evolution_majeure" title="Evolution majeure">Cr&eacute;ation du MCD - MLD de la base de donn&eacute;es.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Cr&eacute;ation d'un script sql d'installation de la base de donn&eacute;es.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Cr&eacute;ation d'un script sql de donn&eacute;es invariables.</li>
+					<li class="evolution_majeure" title="Evolution majeure">Mise en place de la base de donn&eacute;es.</li>
+				</ul>
+			</dd>
+		</dl>
+	</dd>
+</dl>
+
+<h2>LEGENDE</h2>
+<ul>
+	<li class="bug_majeur">Bug majeur</li>
+	<li class="bug_mineur">Bug mineur</li>
+	<li class="bug_faible">Bug faible</li>
+	<li class="evolution_majeure">Evolution majeure</li>
+	<li class="evolution_mineure">Evolution mineure</li>
+	<li class="evolution_faible">Evolution faible</li>
+</ul>
+
+<script type="text/javascript">
+// <![CDATA[
+	/**
+	 * Cache ou affiche la description d'une version
+	 */
+	function showOrHideVersion(objThis, bShow)
+	{
+		// Le titre de la version est dans un "dt"
+		var objVersionHead = null;
+		if(objThis.tagName == 'DT')
+		{
+			objVersionHead = objThis;
+		}
+		else
+		{
+			// Si objThis n'est un "dt" alors on est appelé par l'image, il faut remonter au parent
+			objVersionHead = objThis.parentNode;
+		}
+
+		// La description de la version est dans le "dd" juste à côté (noeud frère)
+		var objVersionBody = objVersionHead.nextSibling;
+
+		// Mais il faut faire attention au texte entre les 2 balises (le retour à la ligne)
+		while(objVersionBody.nodeType != Node.ELEMENT_NODE)
+		{
+			objVersionBody = objVersionBody.nextSibling;
+		}
+
+		// On affiche ou on cache
+		if(typeof(bShow) == 'undefined')
+		{
+			bShow = (objVersionBody.style.display == 'none') ? true : false;
+		}
+
+		objVersionBody.style.display = (bShow == false) ? 'none' : '';
+
+		// On change la source de l'image
+		var objImg = objVersionHead.firstChild;
+
+		// Mais il faut faire attention au texte entre les 2 balises (le retour à la ligne)
+		while(objImg.nodeType != Node.ELEMENT_NODE)
+		{
+			objImg = objImg.nextSibling;
+		}
+
+		if(bShow == true) // Attention on a déjà fait le changement d'affichage
+		{
+			objImg.src   = objImg.src.replace(/_on\./, "_off.");
+			objImg.alt   = "[-]";
+			objImg.title = "Cacher";
+		}
+		else
+		{
+			objImg.src   = objImg.src.replace(/_off\./, "_on.");
+			objImg.alt   = "[+]";
+			objImg.title = "Afficher";
+		}
+
+		return bShow;
+	}
+
+	/**
+	 * Cache ou affiche toutes les sous-versions d'une version
+	 */
+	function showOrHideAllSubVersion(objThis, bShow)
+	{
+		// La description de la version principale est dans un "dd" parent au "a" ou l'on est
+		var objVersionBody = objThis.parentNode;
+
+		var aVersionHeadList = objVersionBody.getElementsByTagName('dt');
+
+		for(i=0; i<aVersionHeadList.length; i++)
+		{
+			objVersionHead = aVersionHeadList[i];
+
+			// Cache ou affiche la version
+			// On recupére l'opération fait à la première version pour appliquer la même autre version
+			bShow = showOrHideVersion(objVersionHead, bShow);
+		}
+	}
+
+	/**
+	 * Cache toutes les versions sauf la dernière
+	 */
+	function hideAllVersionsExceptLast()
+	{
+		var objVersionBlock = document.getElementById('version_block');
+
+		var aVersionHeadList = document.getElementsByTagName('dt', objVersionBlock);
+
+		showOrHideVersion(aVersionHeadList[0], true);
+		showOrHideVersion(aVersionHeadList[0], true);
+
+		for(i=2 ;i<aVersionHeadList.length; i++)
+		{
+			objVersionHead = aVersionHeadList[i];
+
+			showOrHideVersion(objVersionHead, false);
+		}
+	}
+// ]]>
+</script>
+
+<script type="text/javascript">
+// <![CDATA[
+	// ===== Main =====
+
+	// Cache toutes les versions sauf la dernière
+	hideAllVersionsExceptLast();
+// ]]>
+</script>
